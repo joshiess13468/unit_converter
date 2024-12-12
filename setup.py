5af18ca10a9cdbffec432f9267e87c0863fb4366
samueltexa <woolardsamuels@gmail.com>
2024-12-12 16:59:21 +0300

    initial commit

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='unit_converter',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=[],
+    description='A library for converting various units of measurement.',
+    author='Your Name',
+    author_email='your.email@example.com',
+    url='https://github.com/joshiess13468/unit_converter',  # Replace with your actual repository URL
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',  # Change if using a different license
+        'Operating System :: OS Independent',
+    ],
+)
